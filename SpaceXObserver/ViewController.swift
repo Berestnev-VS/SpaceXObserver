9f35861abf348187845bb195526c7fdcc9168a8d
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-29 16:35:01 +0300

    initial code

--- SpaceXObserver/ViewController.swift
+++ SpaceXObserver/ViewController.swift
@@ -9,48 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let rocketsTextView: UITextView = {
-        let textView = UITextView()
-        textView.font = .boldSystemFont(ofSize: 12)
-        textView.textColor = .black
-        return textView
-    }()
-    
     override func viewDidLoad() {
-        rocketsTextView.frame = CGRect(x: .zero, y: .zero, width: view.bounds.width, height: view.bounds.height / 2)
-        view.addSubview(rocketsTextView)
-        
+        super.viewDidLoad()
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        getRockets()
-        getLaunches()
-    }
-    
-    
-    func getRockets(){
-        APICaller.shared.getRockets() { [weak self] result in
-            switch result {
-            case .success(let rockets):
-                DispatchQueue.main.async {
-                    self?.rocketsTextView.text = rockets.description
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    func getLaunches(){
-        APICaller.shared.getLaunches() { result in
-            switch result {
-            case .success(let launches):
-                print(launches)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
 }
