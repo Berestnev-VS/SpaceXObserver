1b51b298dca3e9b9cffd73e63d35e134af5fedcf
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-07-06 11:56:16 +0300

    updated

--- SpaceXObserver/Controllers/SettingsViewController.swift
+++ SpaceXObserver/Controllers/SettingsViewController.swift
@@ -7,33 +7,70 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    let tableView = UITableView()
+final class SettingsViewController: UIViewController {
+    // MARK: - Properties
+    private let tableView = UITableView()
+    private let userDefaultsManager = UserDefaultsManager()
+    private var selectedUnitIndices: [Int] = []
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupTableView()
+        view.addSubview(tableView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupConstraints()
+    }
+
+    // MARK: - Methods
+    private func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
-        tableView.frame = view.bounds
-        view.addSubview(tableView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
+// MARK: - UITableViewDataSource
 extension SettingsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Parameter.allCases.count
+        Parameter.allCases.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
-        cell.parameter = Parameter(rawValue: indexPath.row)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier,
+                                                       for: indexPath) as? SettingsTableViewCell else { return UITableViewCell() }
+        let parameter = Parameter.allCases[indexPath.row]
+        let selectedUnitIndex = userDefaultsManager.getSelectedUnitIndex(for: parameter)
+        cell.configure(with: parameter, selectedUnitIndex: selectedUnitIndex)
+        cell.delegate = self
         return cell
     }
 }
 
+// MARK: - UITableViewDelegate
 extension SettingsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        60
+    }
+}
+
+// MARK: - SettingsTableViewCellDelegate
+extension SettingsViewController: SettingsTableViewCellDelegate {
+    func unitSelectionDidChange(parameter: Parameter, selectedIndex: Int) {
+        userDefaultsManager.saveSelectedUnitIndex(for: parameter, index: selectedIndex)
     }
 }
--- SpaceXObserver/Views/SettingsTableViewCell.swift
+++ SpaceXObserver/Views/SettingsTableViewCell.swift
@@ -7,27 +7,14 @@
 
 import UIKit
 
-class SettingsTableViewCell: UITableViewCell {
+final class SettingsTableViewCell: UITableViewCell {
+    // MARK: - Properties
+    private let unitSegmentedControl = UISegmentedControl()
+    private var parameter: Parameter?
+    static let identifier = String(describing: SettingsTableViewCell.self)
+    weak var delegate: SettingsTableViewCellDelegate?
 
-    static let identifier = "SettingsTableViewCell"
-
-    var parameter: Parameter? {
-        didSet {
-            guard let parameter = parameter else { return }
-            textLabel?.text = parameter.title
-            unitSegmentedControl.removeAllSegments()
-            for (index, unit) in parameter.units.enumerated() {
-                unitSegmentedControl.insertSegment(withTitle: unit, at: index, animated: false)
-            }
-            unitSegmentedControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: parameter.title)
-        }
-    }
-
-    let unitSegmentedControl: UISegmentedControl = {
-        let segmentedControl = UISegmentedControl()
-        return segmentedControl
-    }()
-
+    // MARK: - init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         unitSegmentedControl.addTarget(self, action: #selector(unitChanged), for: .valueChanged)
@@ -39,8 +26,28 @@ class SettingsTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    @objc func unitChanged() {
-        guard let parameter = parameter else { return }
-        UserDefaults.standard.set(unitSegmentedControl.selectedSegmentIndex, forKey: parameter.title)
+    // MARK: - Methods
+    public func configure(with parameter: Parameter, selectedUnitIndex: Int) {
+        self.parameter = parameter
+        textLabel?.text = parameter.title
+        for (index, unit) in parameter.units.enumerated() {
+            unitSegmentedControl.insertSegment(withTitle: unit, at: index, animated: false)
+        }
+        unitSegmentedControl.selectedSegmentIndex = selectedUnitIndex
     }
+
+    @objc private func unitChanged() {
+        guard let parameter = parameter else { return }
+        delegate?.unitSelectionDidChange(parameter: parameter, selectedIndex: unitSegmentedControl.selectedSegmentIndex)
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        unitSegmentedControl.removeAllSegments()
+    }
+}
+
+// MARK: - SettingsTableViewCellDelegate
+protocol SettingsTableViewCellDelegate: AnyObject {
+    func unitSelectionDidChange(parameter: Parameter, selectedIndex: Int)
 }
